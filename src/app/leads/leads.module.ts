8af62ec19c9d7caa0f0603ab8090e35ecdb1de0c
bvallambhotla <balakiran.v@gmail.com>
2021-12-06 22:25:42 +0530

    Added validation for LoginForm and package changes

--- src/app/leads/leads.module.ts
+++ src/app/leads/leads.module.ts
@@ -2,9 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CustomComponentsModule } from '../common/customComponents.module';
 import { HttpClientModule } from '@angular/common/http';
+import { BrowserModule } from '@angular/platform-browser';
 
 @NgModule({
-  imports: [CommonModule, HttpClientModule, CustomComponentsModule],
+  imports: [
+    CommonModule,
+    BrowserModule,
+    HttpClientModule,
+    CustomComponentsModule,
+  ],
   declarations: [],
 })
 export class LeadsModule {}
--- src/app/security/login/login.component.ts
+++ src/app/security/login/login.component.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { USERS_GET } from '../../common/common.constants';
 
@@ -9,6 +10,26 @@ import { USERS_GET } from '../../common/common.constants';
   styleUrls: ['./login.component.css'],
 })
 export class LoginComponent {
+  signinForm = new FormGroup({
+    email: new FormControl('ani@gmail.com', [
+      Validators.required,
+      Validators.email,
+    ]),
+    password: new FormControl('P@ssword', [
+      Validators.required,
+      Validators.minLength(8),
+      Validators.maxLength(16),
+      Validators.pattern('^(?=.*?[A-Z])(?=.*?[#?!@$%^&*-]).{0,}$'),
+    ]),
+  });
+
+  get email() {
+    return this.signinForm.get('email');
+  }
+  get password() {
+    return this.signinForm.get('password');
+  }
+
   constructor(private http: HttpClient, private router: Router) {}
 
   /**
@@ -16,7 +37,7 @@ export class LoginComponent {
    */
   async signin() {
     const users: any = await this.http.get(USERS_GET).toPromise();
-    if (users && users.some((x) => x.email == 'emailfromform')) {
+    if (users && users.some((x) => x.email == this.email.value)) {
       // route to companies
       this.router.navigate(['/companies']);
     } else {
