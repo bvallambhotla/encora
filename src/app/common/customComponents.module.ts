fb2f9c24f76ad7245d7215daf966f8d716bb7f72
bvallambhotla <balakiran.v@gmail.com>
2021-12-07 08:52:41 +0530

    Added toast capability

--- src/app/common/customComponents.module.ts
+++ src/app/common/customComponents.module.ts
@@ -1,10 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SortableHeader } from './directives/sortableHeader.directive';
+import { ToastService } from './components/toast.service';
+import { ToastsContainer } from './components/toast-container.component';
+import { NgbToastModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
-  imports: [CommonModule],
-  declarations: [SortableHeader],
-  exports: [SortableHeader],
+  imports: [CommonModule, NgbToastModule],
+  declarations: [SortableHeader, ToastsContainer],
+  exports: [SortableHeader, ToastsContainer],
+  providers: [ToastService],
 })
 export class CustomComponentsModule {}
